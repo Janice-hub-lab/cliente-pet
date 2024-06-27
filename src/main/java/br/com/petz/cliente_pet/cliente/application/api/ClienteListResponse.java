55327d9f070730ff00afafefbdab06e1ffb28474
Jane Gomes <asscontecjane@gmail.com>
2024-06-27 17:44:27 -0300

    implementa fluxo api, teste ok!

---
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteListResponse.java
@@ -0,0 +1,14 @@
+package br.com.petz.cliente_pet.cliente.application.api;
+
+import java.util.UUID;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class ClienteListResponse {
+	private UUID idCliente;
+	private String nomeCompleto;
+	private String cpf;
+	private String email;
+	private String celular;
+
+}
