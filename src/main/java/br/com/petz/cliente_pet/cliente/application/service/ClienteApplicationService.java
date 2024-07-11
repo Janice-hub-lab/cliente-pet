9eb22c4ad06d29bf24d69d6c6da1fee49ad7e2d3
Jane Gomes <asscontecjane@gmail.com>
2024-07-11 18:57:26 -0300

    implementa fluxo de servico de aplicacao

--- src/main/java/br/com/petz/cliente_pet/cliente/application/service/ClienteApplicationService.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/service/ClienteApplicationService.java
@@ -1,9 +1,11 @@
 package br.com.petz.cliente_pet.cliente.application.service;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import br.com.petz.cliente_pet.cliente.application.api.ClienteDetalhadoResponse;
 import br.com.petz.cliente_pet.cliente.application.api.ClienteListResponse;
 import br.com.petz.cliente_pet.cliente.application.api.ClienteRequest;
 import br.com.petz.cliente_pet.cliente.application.api.ClienteResponse;
@@ -36,6 +38,13 @@ public class ClienteApplicationService implements ClienteService {
 		List<Cliente> clientes = clienteRepository.buscaTodosCliente();
 		log.info("[finaliza] ClienteApplicationService - buscaTodosClientes");
 		return ClienteListResponse.converte(clientes);
+	}
+
+	@Override
+	public ClienteDetalhadoResponse buscaClienteAtravesId(UUID idCliente) {
+		log.info("[inicia] ClienteApplicationService - buscaClienteAtravesId");
+		log.info("[finaliza] ClienteApplicationService - buscaClienteAtravesId");
+		return null;
 	}
 }
 
