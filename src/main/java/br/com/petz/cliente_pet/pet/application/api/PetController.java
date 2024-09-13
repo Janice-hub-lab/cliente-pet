03259bb7e2d29d1d4a99de1d46f14cff2db4b3ba
Jane Gomes <asscontecjane@gmail.com>
2024-09-13 15:08:52 -0700

    implementa fluxo da api

--- src/main/java/br/com/petz/cliente_pet/pet/application/api/PetController.java
+++ src/main/java/br/com/petz/cliente_pet/pet/application/api/PetController.java
@@ -1,5 +1,6 @@
 package br.com.petz.cliente_pet.pet.application.api;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,14 @@ public class PetController implements PetAPI {
 		PetResponse pet = petService.criaPet(idCliente, petRequest);
 		log.info("[finaliza] PetController - postPet");
 		return pet;
+	}
+
+	@Override
+	public List<petClienteListResponse> getPetDoCliente(UUID idCliente) {
+		log.info("[inicia] PetController - getPetDoCliente");
+		log.info("[idCliente] {}", idCliente );
+		log.info("[finish] PetController - getPetDoCliente");
+		return null;
 	}
 
 }
