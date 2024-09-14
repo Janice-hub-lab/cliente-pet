33331b26d921e692633106dabcd89ce35f57bcfd
Jane Gomes <asscontecjane@gmail.com>
2024-09-13 20:05:09 -0700

    implementa fluxo de camada de api

---
+++ src/main/java/br/com/petz/cliente_pet/pet/application/api/PetClienteDetalheResponse.java
@@ -0,0 +1,32 @@
+package br.com.petz.cliente_pet.pet.application.api;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import br.com.petz.cliente_pet.pet.domain.Porte;
+import br.com.petz.cliente_pet.pet.domain.SexoPet;
+import br.com.petz.cliente_pet.pet.domain.TipoPet;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+
+@Value
+public class PetClienteDetalheResponse {
+	private UUID idPet;
+	private UUID idClienteTutor;
+	private String nomePet;	
+	private Porte porte;	
+	private TipoPet tipo;
+	private String microchip;
+	private String raca;	
+	private SexoPet sexo;
+	private String pelagemCor;
+	private LocalDate dataNascimento;
+	private String rga;
+	private Integer peso;
+
+}
